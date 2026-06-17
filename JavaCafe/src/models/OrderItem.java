22dacf401ca1e5761d41b142bdd1be7c4b008095
giovanamarmo <giovanamarmo2005@gmail.com>
2026-06-17 10:48:23 -0300

    Update models

---
+++ JavaCafe/src/models/OrderItem.java
@@ -0,0 +1,30 @@
+package models;
+
+import exceptions.OutOfStockException;
+
+public class OrderItem {
+	private Product product;
+    private int quantity;
+
+    public OrderItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    // Retorna o valor total deste item específico
+    public double getSubtotal() {
+        return product.getPrice() * quantity;
+    }
+
+    // Getters e Setters
+    public Product getProduct() { return product; }
+    public void setProduct(Product product) { this.product = product; }
+    
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+    
+    @Override
+    public String toString() {
+        return quantity + "x " + product.getName() + " - R$ " + String.format("%.2f", getSubtotal());
+    }
+}
